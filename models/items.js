7ef1a049a36d346cfc20d40c94b7abb165d00418
Timothy Mickle <timothyrmickle@gmail.com>
2018-07-11 15:09:14 -0500

    Fixed images rendering,addedhome button for show.ejs, added show.ejs rendering

--- models/items.js
+++ models/items.js
@@ -2,19 +2,19 @@ const items = [
   {
       name:'spinach',
       type: 'vegetable',
-      edible: true
+      edible: true,
       img: "https://media.mercola.com/assets/images/foodfacts/spinach2-fb.jpg",
   },
   {
   name:'rose',
   type: 'wine',
-  edible: false
+  edible: false,
   img: "https://wine-searcher1.freetls.fastly.net/images/labels/52/29/domaine-armand-rousseau-pere-et-fils-chambertin-clos-de-beze-grand-cru-cote-de-nuits-france-10155229.jpg"
   },
   {
   name:'black beans',
   type: 'condiment',
-  edible: true
+  edible: true,
   img: "http://cdn-img.health.com/sites/default/files/styles/master_4_3/public/styles/main/public/black-beans-challenge-400x400.jpg?itok=rAzmgWYy"
   }
 ];
--- server.js
+++ server.js
@@ -4,16 +4,20 @@ const app = express();
 
 const port = 3000;
 
-const Items = require('/items');
+const Items = require('./models/items');
 
 app.get('/fridge', (req,res)=> {
   res.render('index.ejs', {
-    items = Items
+    items: Items
   });
   // res.send('This is something in a string inside some ')
 })
 
-app.get('')
+app.get('/fridge/:id', (req, res)=>{
+  res.render('show.ejs', {
+    item: Items[req.params.id]
+  });
+})
 
 app.listen(port,()=>{
     console.log('Listening on port 3000')
