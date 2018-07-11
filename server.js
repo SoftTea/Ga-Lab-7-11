766f32a9b0c229282e38597fdd0c0237193449fc
Timothy Mickle <timothyrmickle@gmail.com>
2018-07-11 15:50:41 -0500

    now can add something, channged name for 'image'  tag in input tag add.ejs to 'img'

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const app = express();
 
 const port = 3000;
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 const Items = require('./models/items');
+
+
 
 app.get('/fridge', (req,res)=> {
   res.render('index.ejs', {
@@ -13,11 +21,28 @@ app.get('/fridge', (req,res)=> {
   // res.send('This is something in a string inside some ')
 })
 
+app.get('/fridge/add', (req,res)=> {
+  res.render('add.ejs',{
+
+  })
+
+})
+app.post('/fridge', (req,res)=> {
+  Items.push(req.body);
+  console.log(req.body);
+  // console.log(Items);
+  res.redirect('/fridge');
+})
+
 app.get('/fridge/:id', (req, res)=>{
   res.render('show.ejs', {
     item: Items[req.params.id]
   });
 })
+
+
+
+
 
 app.listen(port,()=>{
     console.log('Listening on port 3000')
