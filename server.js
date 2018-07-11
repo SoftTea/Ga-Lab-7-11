c7ded2056a7739a582ca116bba4099e03536d0af
Timothy Mickle <timothyrmickle@gmail.com>
2018-07-11 16:28:49 -0500

    added edit page

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const port = 3000;
 
 const bodyParser = require('body-parser');
 
+const methodOverride = require('method-override');
+
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+app.use(methodOverride('_method'));
 
 const Items = require('./models/items');
 
@@ -27,10 +31,39 @@ app.get('/fridge/add', (req,res)=> {
   })
 
 })
+
+
 app.post('/fridge', (req,res)=> {
   Items.push(req.body);
   console.log(req.body);
   // console.log(Items);
+  res.redirect('/fridge');
+})
+
+app.put('/fridge/:id', (req, res) => {
+  if(req.body.edible === 'on'){
+  req.body.edible = true;
+} else {
+  req.body.edible = false;
+}
+
+Items[req.params.id] = req.body;
+res.redirect('/fridge');
+});
+
+app.get('/fridge/:id/edit', (req, res) => {
+
+  res.render('edit.ejs', {
+    item: Items[req.params.id],
+    id: req.params.id
+  
+  });
+ 
+  })
+
+app.delete('/fridge/:id', (req, res) => {
+  Items.splice(req.params.id, 1);
+  console.log(req.params.id, ' this is req.params')
   res.redirect('/fridge');
 })
 
